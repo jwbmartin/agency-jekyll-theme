9703f479f72a444e1a6b8fee8e34575d1fca8995
Jordan W Martin <jordan@zooniverse.org>
2016-05-20 08:36:18 -0500

    style updates

--- img/sketch.js
+++ img/sketch.js
@@ -24,30 +24,6 @@ function draw() {
         ellipse(i, j, random(1), random(20));
       }
     }
- } else if (touchIsDown) {
-  background(31,247,183);
-    fill(225, 255, 255);
-    stroke(35,79,163);
-    strokeWeight(1.5);
-    var dist = 90;
-    for (var i = 25; i < width; i+=dist) 
-    {
-      for (var j = 25; j < height; j+=dist) 
-      {
-        rectMode(CENTER);
-        ellipse(i, j, 40, 20);
-      }
-    }
-    fill(35,79,163);
-    for (var i = 25; i < width; i+=dist) 
-    {
-      for (var j = 25; j < height; j+=dist) 
-      {
-        var x1 = map(touchX, 0, width, i-11.5, 11.5+i);
-        var y1 = map(touchY, 0, height, j-3.2, j+3);
-        ellipse(x1, y1, 14, 14);
-      }
-    }
  } else {
  	background(31,247,183);
     fill(225, 255, 255);
