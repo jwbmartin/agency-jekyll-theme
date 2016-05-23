211a3361d66c86e22969631b3f45b30ab53074f4
Jordan W Martin <jordan@zooniverse.org>
2016-05-23 14:23:53 -0500

    edit sketch

--- img/sketch.js
+++ img/sketch.js
@@ -29,23 +29,23 @@ function draw() {
     fill(225, 255, 255);
     stroke(35,79,163);
     strokeWeight(1.5);
-    var dist = 128;
-    for (var i = 30; i < width; i+=dist) 
+    var dist = 90;
+    for (var i = 25; i < width; i+=dist) 
     {
-      for (var j = 30; j < height; j+=dist) 
+      for (var j = 25; j < height; j+=dist) 
       {
         rectMode(CENTER);
-        ellipse(i, j, 80, 40);
+        ellipse(i, j, 40, 22);
       }
     }
     fill(35,79,163);
-    for (var i = 30; i < width; i+=dist) 
+    for (var i = 25; i < width; i+=dist) 
     {
-      for (var j = 30; j < height; j+=dist) 
+      for (var j = 25; j < height; j+=dist) 
       {
         var x1 = map(mouseX || touchX, 0, width, i-11.5, 11.5+i);
         var y1 = map(mouseY || touchY, 0, height, j-3.2, j+3);
-        ellipse(x1, y1, 30, 30);
+        ellipse(x1, y1, 14, 14);
       }
     }
   }
