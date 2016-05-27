ecaab6c1a6cefc69a5a562646a86cd008eb50fcd
Jordan W Martin <jordan@zooniverse.org>
2016-05-26 19:58:33 -0500

    editing sketch mobile

--- js/sketch.js
+++ js/sketch.js
@@ -1,39 +1,45 @@
+var bg = $('#bg');
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+ var canvas = createCanvas(bg.width(), bg.height());
+  canvas.parent('bg');
   smooth();
   frameRate(8);
+
 }
 
 function draw() {
   if (mouseIsPressed) {
-  	background("deepPink");
+    background(237,34,93);
     fill(255,255,255);
     stroke(35,79,163);
-      	var dist = 85;
+    strokeWeight(1.5);
+        var dist = 90;
     for (var i = 25; i < width; i+=dist) {
       for (var j = 25; j < height; j+=dist) {
         rectMode(CENTER);
-        ellipse(i, j, random(30), random(20));
+        ellipse(i, j, random(40), random(25));
       }
     }
     fill(35,79,163);
     for (var i = 25; i < width; i+=dist) {
       for (var j = 25; j < height; j+=dist) {
         rectMode(CENTER);
-        ellipse(i, j, random(10), random(20));
+        ellipse(i, j, random(1), random(20));
       }
     }
  } else {
- 	background(31,247,183);
+  background(31,247,183);
     fill(225, 255, 255);
     stroke(35,79,163);
-    var dist = 85;
+    strokeWeight(1.5);
+    var dist = 90;
     for (var i = 25; i < width; i+=dist) 
     {
       for (var j = 25; j < height; j+=dist) 
       {
         rectMode(CENTER);
-        ellipse(i, j, 30, 14);
+        ellipse(i, j, 40, 20);
       }
     }
     fill(35,79,163);
@@ -41,9 +47,9 @@ function draw() {
     {
       for (var j = 25; j < height; j+=dist) 
       {
-        var x1 = map(mouseX, 0, width, i-10, 10+i);
-        var y1 = map(mouseY, 0, height, j-1, j+3);
-        ellipse(x1, y1, 8, 8);
+        var x1 = map(mouseX, 0, width, i-11.5, 11.5+i);
+        var y1 = map(mouseY, 0, height, j-3.2, j+3);
+        ellipse(x1, y1, 14, 14);
       }
     }
   }
