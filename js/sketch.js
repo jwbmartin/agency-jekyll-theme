140ef9d46e7f88a6ecd1166156b5469721cd2e62
Jordan W Martin <jordan@zooniverse.org>
2016-05-27 08:59:05 -0500

    edit js

--- js/sketch.js
+++ js/sketch.js
@@ -1,20 +1,22 @@
+//OpenProcessing Tweak of http://www.openprocessing.org/sketch/70750 written by Jordan Whitney Martin
+
 var bg = $('#bg');
 
 function setup() {
  var canvas = createCanvas(bg.width(), bg.height());
   canvas.parent('bg');
   smooth();
-  frameRate(8);
+  frameRate(13);
 
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    background(237,34,93);
+  if (mouseIsPressed || touchIsDown) {
+  	background(237,34,93);
     fill(255,255,255);
     stroke(35,79,163);
     strokeWeight(1.5);
-        var dist = 90;
+      	var dist = 90;
     for (var i = 25; i < width; i+=dist) {
       for (var j = 25; j < height; j+=dist) {
         rectMode(CENTER);
@@ -29,7 +31,7 @@ function draw() {
       }
     }
  } else {
-  background(31,247,183);
+ 	background(31,247,183);
     fill(225, 255, 255);
     stroke(35,79,163);
     strokeWeight(1.5);
@@ -39,7 +41,7 @@ function draw() {
       for (var j = 25; j < height; j+=dist) 
       {
         rectMode(CENTER);
-        ellipse(i, j, 40, 20);
+        ellipse(i, j, 40, 22);
       }
     }
     fill(35,79,163);
@@ -47,8 +49,8 @@ function draw() {
     {
       for (var j = 25; j < height; j+=dist) 
       {
-        var x1 = map(mouseX, 0, width, i-11.5, 11.5+i);
-        var y1 = map(mouseY, 0, height, j-3.2, j+3);
+        var x1 = map(mouseX || touchX, 0, width, i-11.5, 11.5+i);
+        var y1 = map(mouseY || touchY, 0, height, j-3.2, j+3);
         ellipse(x1, y1, 14, 14);
       }
     }
