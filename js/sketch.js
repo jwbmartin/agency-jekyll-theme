a75caa4174b57899c18a7e828153a02cb74563e5
Jordan W Martin <jordan@zooniverse.org>
2016-08-31 14:11:32 -0500

    new styling

--- js/sketch.js
+++ js/sketch.js
@@ -11,48 +11,29 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed || touchIsDown) {
-  	background(237,34,93);
-    fill(255,255,255);
-    stroke(35,79,163);
-    strokeWeight(1.5);
-      	var dist = 90;
-    for (var i = 25; i < width; i+=dist) {
-      for (var j = 25; j < height; j+=dist) {
-        rectMode(CENTER);
-        ellipse(i, j, random(40), random(25));
-      }
-    }
-    fill(35,79,163);
-    for (var i = 25; i < width; i+=dist) {
-      for (var j = 25; j < height; j+=dist) {
-        rectMode(CENTER);
-        ellipse(i, j, random(1), random(20));
-      }
-    }
- } else {
- 	background(31,247,183);
-    fill(225, 255, 255);
-    stroke(35,79,163);
-    strokeWeight(1.5);
-    var dist = 90;
+ 	background(243,243,243);
+    fill(243, 243, 243);
+    stroke(30,247,183);
+    strokeWeight(1);
+    var dist = 65;
     for (var i = 25; i < width; i+=dist) 
     {
       for (var j = 25; j < height; j+=dist) 
       {
         rectMode(CENTER);
-        ellipse(i, j, 40, 22);
+        ellipse(i, j, 30, 14);
       }
     }
-    fill(35,79,163);
+    fill(30,247,183);
+    strokeWeight (2);
     for (var i = 25; i < width; i+=dist) 
     {
       for (var j = 25; j < height; j+=dist) 
       {
-        var x1 = map(mouseX || touchX, 0, width, i-11.5, 11.5+i);
+        var x1 = map(mouseX || touchX, 0, width, i-10, 10+i);
         var y1 = map(mouseY || touchY, 0, height, j-2, j+2);
-        ellipse(x1, y1, 14, 14);
+        ellipse(x1, y1, 8, 8);
       }
     }
   }
-}
+ 
